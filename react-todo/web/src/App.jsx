2837aa8a9e632e23a449b64a6c1702523cf08b97
Elon Musk <ireish@hotmail.com>
2025-09-07 13:41:39 -0700

    feat: add searchbox component

--- react-todo/web/src/App.jsx
+++ react-todo/web/src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
+import './component/SearchBox'
 import './App.css'
+import { SearchBox } from './component/SearchBox'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <h1>TO-DO App</h1>
+      <SearchBox/>
     </>
   )
 }
---
+++ react-todo/web/src/component/SearchBox.tsx
@@ -0,0 +1,7 @@
+export function SearchBox() {
+    return (
+        <>
+          <input type="text" id="search-input" placeholder="Enter a task . . ."/>
+        </>
+    )
+}
