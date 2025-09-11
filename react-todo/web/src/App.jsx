38a6a013633db042ee8dd1cf6e36ab2d51ea2d64
Elon Musk <ireish@hotmail.com>
2025-09-10 23:59:36 -0700

    refactor: add TodoApp component

--- react-todo/web/src/App.jsx
+++ react-todo/web/src/App.jsx
@@ -1,15 +1,10 @@
-import { useState } from 'react'
-import './component/SearchBox'
 import './App.css'
-import { SearchBox } from './component/SearchBox'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <h1>TO-DO App</h1>
-      <SearchBox/>
+      <TodoApp/>
     </>
   )
 }
---
+++ react-todo/web/src/component/TodoApp.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './component/SearchBox'
+import './App.css'
+import { SearchBox } from './component/SearchBox'
+
+function App() {
+  
+  return (
+    <>
+      <h1>TO-DO App</h1>
+      <SearchBox/>
+    </>
+  )
+}
+
+export default App
