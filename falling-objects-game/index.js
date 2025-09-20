061681103874328fa4533355a99a0a133d3dc226
Elon Musk <ireish@hotmail.com>
2025-09-20 16:11:13 -0700

    refactor: make scalable; add player class for player related funcs

--- falling-objects-game/index.js
+++ falling-objects-game/index.js
@@ -5,24 +5,58 @@ const ctx = canvas.getContext("2d");
 const canvasHeight = canvas.height
 const canvasWidth = canvas.width
 
-let PLAYER_X = 150, PLAYER_Y = 580;
-const PLAYER_SPEED = 10
+const PLAYER_WIDTH = 20;
+const PLAYER_HEIGHT = 20;
+const PLAYER_SPEED = 8;
 
-const pressedKeys = {}
+const PLAYER_START_X = (canvasWidth / 2) - (PLAYER_WIDTH / 2);
+const PLAYER_START_Y = canvasHeight - PLAYER_HEIGHT;
+
+const PRESSED_KEYS = {}
 
 
 function drawCanvas() {
-    ctx.clearRect(0, 0, canvasWidth, canvasHeight)
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
     ctx.fillStyle = "#c3cdd6";
     ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 }
 
 
-function initPlayer(PLAYER_X, PLAYER_Y) {
-    ctx.fillStyle = "#bb333fff";
-    ctx.fillRect(PLAYER_X, PLAYER_Y, 20, 20);
+// =========================   PLAYER LOGIC   =========================
+
+class Player {
+    constructor(x, y, speed) {
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+    }
+
+    movePlayer() {
+        if (PRESSED_KEYS["ArrowLeft"] || PRESSED_KEYS["a"]) {
+            this.x -= this.speed;
+        }
+        if (PRESSED_KEYS["ArrowRight"] || PRESSED_KEYS["d"]) {
+            this.x += this.speed;
+        }
+        if (PRESSED_KEYS["ArrowUp"] || PRESSED_KEYS["w"]) {
+            this.y -= this.speed;
+        }
+        if (PRESSED_KEYS["ArrowDown"] || PRESSED_KEYS["s"]) {
+            this.y += this.speed;
+        }
+
+        // stay inside canvas boundary
+        this.x = Math.min(Math.max(0, this.x), canvasWidth - PLAYER_WIDTH);
+        this.y = Math.min(Math.max(0, this.y), canvasHeight - PLAYER_HEIGHT);
+
+        // update position
+        ctx.fillStyle = "#bb333fff";
+        ctx.fillRect(this.x, this.y, PLAYER_WIDTH, PLAYER_HEIGHT);
+    }
 }
 
+
+// =========================   FALLING BLOCKS   =========================
 
 class Block {
     constructor() {
@@ -31,6 +65,7 @@ class Block {
         this.speed = getRandomSpeed();
     }
 }
+
 
 class FallingBlocks {
 
@@ -47,10 +82,7 @@ class FallingBlocks {
         }
     }
 
-
-    initFall() {
-        drawCanvas();
-        initPlayer(PLAYER_X, PLAYER_Y);
+    updatePosition() {
 
         for (let i = 0; i < this.blocks.length; i++) {
 
@@ -69,79 +101,55 @@ class FallingBlocks {
                 this.blocks[i].speed = getRandomSpeed();
             }
         }
-
-        requestAnimationFrame(() => this.initFall())
     }
+
 }
-
-
-// }
-
-function movePlayer() {
-
-    if(pressedKeys["ArrowLeft"] || pressedKeys["a"]) {
-        PLAYER_X -= PLAYER_SPEED;
-    }
-    if(pressedKeys["ArrowRight"] || pressedKeys["d"]) {
-        PLAYER_X += PLAYER_SPEED;
-    }
-    if(pressedKeys["ArrowUp"] || pressedKeys["w"]) {
-        PLAYER_Y -= PLAYER_SPEED;
-    }
-    if(pressedKeys["ArrowDown"] || pressedKeys["s"]) {
-        PLAYER_Y += PLAYER_SPEED;
-    }
-
-    // stay inside canvas boundary
-    PLAYER_X = Math.min( Math.max(1, PLAYER_X), canvasWidth - 21);
-    PLAYER_Y = Math.min( Math.max(1, PLAYER_Y), canvasHeight - 21);
-
-    requestAnimationFrame(movePlayer);
-}
-
 
 
 // =========================   UTIL FUNCTIONS   =========================
 
 function getRandomX() {
     let val = Math.random();
-    return Math.floor(val * 270)
+    return Math.floor(val * (canvasWidth - PLAYER_WIDTH))
 }
 
 function getRandomSpeed() {
     let val = Math.random();
-    return Math.ceil(val * 4) + 4
+    return Math.ceil(val * 5) + 4
 }
 
 
 
-// =========================   MAIN GAME INIT   =========================
+// =========================   MAIN GAME LOOP   =========================
 
-drawCanvas();
-initPlayer(PLAYER_X, PLAYER_Y);
-// initFallingBlocks(30);
-let fallingBlocksObj = new FallingBlocks();
-fallingBlocksObj.initFall();
+function gameLoop() {
 
-movePlayer();
+    drawCanvas();
 
+    player.movePlayer();
 
+    fallingBlocks.updatePosition();
+
+    requestAnimationFrame(gameLoop)
+}
+
+const player = new Player(PLAYER_START_X, PLAYER_START_Y, PLAYER_SPEED);
+const fallingBlocks = new FallingBlocks();
+
+gameLoop();
 
 
 // =========================   EVENT LISTNERS   =========================
 
-
-
-
 document.addEventListener('keydown', function (event) {
 
-    pressedKeys[event.key] = true;
+    PRESSED_KEYS[event.key] = true;
 
 });
 
 
 document.addEventListener('keyup', function (event) {
 
-    pressedKeys[event.key] = false;
+    PRESSED_KEYS[event.key] = false;
 
 });
